1c69d9fe8952657e74b96a783d178c3019935aff
aaronbgrg-dot <aaronbgrg@gmail.com>
2026-01-29 08:38:39 +0100

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,10 +2,11 @@ from precios import comparar_pieza_coche
 
 def ejecutar_comparador():
     print("\nSIMULADOR DE MERCADO DE PIEZAS\n")
-
+# Solicita los datos al usuario
     coche = input("Introduce el nombre del coche: ")
     pieza = input("Introduce la pieza: ")
     ofertas = input("Número de ofertas a comparar (por defecto 5): ")
+  # Si el usuario no introduce nada, se usan 5 ofertas por defecto
 
     if ofertas.strip() == "":
         ofertas = 5
@@ -18,13 +19,13 @@ def ejecutar_comparador():
         return
 
     precios, mejor_precio = resultado
-
+  # Muestra las ofertas encontradas
     print(f"\nOfertas encontradas para {pieza} ({coche}):")
     contador = 1
     for precio in precios:
         print(f"Vendedor {contador}: {precio} €")
         contador += 1
-
+# Muestra el mejor precio
     print(f"\nMEJOR PRECIO: {mejor_precio} €")
 
 
